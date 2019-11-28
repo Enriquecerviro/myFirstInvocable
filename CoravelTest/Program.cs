ab2e2cf3c559f223926e96ceb19321386c640236
Enriquecerviro <32262333+Enriquecerviro@users.noreply.github.com>
2019-11-28 17:27:52 +0100

    hola

--- CoravelTest/Program.cs
+++ CoravelTest/Program.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Coravel;
+using CoravelTest.Workers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -11,14 +13,22 @@ namespace CoravelTest
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            IHost host = CreateHostBuilder(args).Build();
+            host.Services.UseScheduler(scheduler =>
+            {
+                scheduler
+                .Schedule<firstWorker>()
+                .EveryFiveSeconds();
+            });
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureServices((hostContext, services) =>
                 {
-                    services.AddHostedService<Worker>();
+                    services.AddScheduler();
+                    services.AddTransient<firstWorker>();
                 });
     }
 }
---
+++ CoravelTest/Workers/firstWorker.cs
@@ -0,0 +1,31 @@
+﻿using Coravel.Invocable;
+using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Persistence.Repositories;
+
+namespace CoravelTest.Workers
+{
+    public class firstWorker : IInvocable
+    {
+        private IMailer _mailer;
+        private IUserRepository _repo;
+
+        public SendDailyReportEmailJob(IMailer mailer, IUserRepository repo)
+        {
+            this._mailer = mailer;
+            this._repo = repo;
+        }
+        public async Task Invoke()
+        {
+            var users = await this._repo.GetUsersAsync();
+            foreach(var user in users)
+            {
+                var mailable = new DailyReportMailable(user);
+                await this._mailer.SendAsync(mailable);
+            }
+        }
+    }
+}
